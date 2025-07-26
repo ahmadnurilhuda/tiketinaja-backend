2761ef2afe33b93ba58ba52f130510fab6f5ee4d
Ahmad Nuril Huda <ahmadnurilhuda18@gmail.com>
2025-07-26 19:13:43 +0700

    first commit

---
+++ src/main/java/com/greenacademy/tiketinaja/TiketinajaApplication.java
@@ -0,0 +1,13 @@
+package com.greenacademy.tiketinaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiketinajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiketinajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenacademy/tiketinaja/TiketinajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenacademy.tiketinaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketinajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
