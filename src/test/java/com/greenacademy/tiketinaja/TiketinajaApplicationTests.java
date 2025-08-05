3756a5007226405db999a04c29e2bff96fb7ce42
Ahmad Nuril Huda <ahmadnurilhuda18@gmail.com>
2025-08-05 17:48:29 +0700

    Update variable in .env

--- src/test/java/com/greenacademy/tiketinaja/TiketinajaApplicationTests.java
+++ src/test/java/com/greenacademy/tiketinaja/TiketinajaApplicationTests.java
@@ -2,8 +2,10 @@ package com.greenacademy.tiketinaja;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class TiketinajaApplicationTests {
 
 	@Test
